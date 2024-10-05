9af73a6f5e8da23549e02b0a04d7c59088060f49
TanmaySayareLenovo <144255204+Tanmay-say@users.noreply.github.com>
2024-10-05 14:42:08 +0530

    Done All Screens

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './Components/HomeScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons"; // Expo Icons
 
-const Stack = createStackNavigator();
+// Import your screens
+import HomePage from "./Components/Home";
+import WishlistPage from "./Components/Wishlist";
+import CategoriesPage from "./Components/Categories";
+import Notification from "./Components/Notification";
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Wishlist") {
+              iconName = focused ? "heart" : "heart-outline";
+            } else if (route.name === "Categories") {
+              iconName = focused ? "ticket" : "ticket-outline";
+            } else if (route.name === "Notification") {
+              iconName = focused ? "newspaper" : "newspaper-outline";
+            } else if (route.name === "Profile") {
+              iconName = focused ? "person" : "person-outline";
+            }
+
+            // Return the icon component
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "#ff6600",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="Wishlist" component={WishlistPage} />
+        <Tab.Screen name="Categories" component={CategoriesPage} />
+        <Tab.Screen name="Notification" component={Notification} />
+        <Tab.Screen name="Profile" component={Notification} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
